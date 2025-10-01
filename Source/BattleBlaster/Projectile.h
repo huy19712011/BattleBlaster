a4249a39bb711b1c7a8f465ce0e0c73591ca139d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-01 23:09:21 +0700

    v123: Health Component

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -32,6 +32,9 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	UProjectileMovementComponent* ProjectileMovementComp;
 
+	UPROPERTY(EditAnywhere)
+	float Damage = 25.0f;
+	
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent,
 		AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
